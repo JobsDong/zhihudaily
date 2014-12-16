0a10d61fca3f2c138042dfd8ec420499e775a31c
wuyadong <wuyadong311521@gmail.com>
2014-12-16 13:17:15 +0800

    complete chinese search

--- search.py
+++ search.py
@@ -6,35 +6,58 @@
 
 __author__ = ['"wuyadong" <wuyadong311521@gmail.com>']
 
-from whoosh.index import open_dir
-from whoosh.fields import Schema, TEXT, ID, DATETIME
-from whoosh.qparser import QueryParser
+from whoosh.index import open_dir, create_in, exists_in
+from whoosh.fields import Schema, TEXT, ID
+from whoosh.qparser import MultifieldParser
+from jieba.analyse import ChineseAnalyzer
 
 
 class FTS(object):
 
     def __init__(self, index_path):
-        schema = Schema(path=ID(stored=True), title=TEXT,
-                        content=TEXT, date=DATETIME)
-        self._ix = open_dir(index_path, schema)
-        self._writer = self._ix.writer()
-        self._searcher = self._ix.searcher()
-        self._parser = QueryParser("content", self._ix.schema)
+        analyzer = ChineseAnalyzer()
+        schema = Schema(path=ID(unique=True, stored=True),
+                        title=TEXT(analyzer=analyzer),
+                        content=TEXT(analyzer=analyzer))
+        if exists_in(index_path):
+            self._ix = open_dir(index_path, schema=schema)
+        else:
+            self._ix = create_in(index_path, schema=schema)
+        self._parser = MultifieldParser(["title", "content"], self._ix.schema)
 
     def search(self, query_string, limit=10):
         """搜索文件
         """
-        query = self._parser.parse(query_string)
-        results = self._searcher.search(query)
-        return results
+        # convert to unicode
+        query_string = str2unicode(query_string)
 
-    def add_doc(self, path, title=None, content=None, date=None):
+        with self._ix.searcher() as s:
+            query = self._parser.parse(query_string)
+            search_results = s.search(query, limit=limit)
+
+        return search_results
+
+    def add_doc(self, path, title=None, content=None):
         """增加文件
         """
-        self._writer.add_document(path=path, title=title,
-                                  content=content, date=date)
-        self._writer.commit()
+        writer = self._ix.writer()
+        path = str2unicode(path)
+        title = str2unicode(title)
+        content = str2unicode(content)
+        writer.add_document(path=path, title=title,
+                            content=content)
+        writer.commit(optimize=True, merge=True)
 
-    def close(self):
-        if self._searcher:
-            self._searcher.close()
+    def add_many_doc(self, docs=None):
+        writer = self._ix.writer()
+        docs = docs or []
+        for doc in docs:
+            if 'path' in doc and 'title' in doc and 'content' in doc:
+                writer.add_document(path=str2unicode(doc['path']),
+                                    title=str2unicode(doc['title']),
+                                    content=str2unicode(doc['content']))
+        writer.commit(optimize=True, merge=True)
+
+
+def str2unicode(text):
+    return text.decode('utf-8') if isinstance(text, str) else text
