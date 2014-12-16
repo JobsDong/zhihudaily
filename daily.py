e4e5e51e972a9e9c5762eab60881d5cc8151ddc4
wuyadong <wuyadong311521@gmail.com>
2014-12-16 10:51:55 +0800

    remove \t

--- daily.py
+++ daily.py
@@ -13,70 +13,70 @@ import json
 
 class ZhiHuServiceException(Exception):
 
-	def __init__(self, status, msg, err):
-		self.args = (status, msg, err)
-		self.status = status
-		self.msg = msg
-		self.err = err
+    def __init__(self, status, msg, err):
+        self.args = (status, msg, err)
+        self.status = status
+        self.msg = msg
+        self.err = err
 
 
 class ZhiHuClientException(Exception):
 
-	def __init__(self, msg):
-		super(ZhiHuClientException, self).__init__(msg)
+    def __init__(self, msg):
+        super(ZhiHuClientException, self).__init__(msg)
 
 
 class ZhiHu(object):
-	"""知乎日报的SDK类
-	"""
-	def __init__(self):
-		self._host = "news-at.zhihu.com"
-		self._port = 80
-		self._timeout = 10
+    """知乎日报的SDK类
+    """
+    def __init__(self):
+        self._host = "news-at.zhihu.com"
+        self._port = 80
+        self._timeout = 10
 
-	def get_latest_news(self):
-		content = self._do_http_request("/api/3/news/latest")
-		return json.loads(content)
+    def get_latest_news(self):
+        content = self._do_http_request("/api/3/news/latest")
+        return json.loads(content)
 
-	def get_news(self, news_id):
-		content = self._do_http_request("/api/3/news/%s" % news_id)
-		return json.loads(content)
+    def get_news(self, news_id):
+        content = self._do_http_request("/api/3/news/%s" % news_id)
+        return json.loads(content)
 
-	def get_before_news(self, date_str):
-		content = self._do_http_request("/api/3/news/before/%s" % date_str)
-		return json.loads(content)
+    def get_before_news(self, date_str):
+        content = self._do_http_request("/api/3/news/before/%s" % date_str)
+        return json.loads(content)
 
-	def _decode_msg(self, msg):
-		if isinstance(msg, str):
-			msg = unicode(msg, 'utf-8')
+    def _decode_msg(self, msg):
+        if isinstance(msg, str):
+            msg = unicode(msg, 'utf-8')
 
-		return msg
+        return msg
 
-	def _do_http_request(self, url):
-		http, content, msg, error, status = None, None, None, None, None
-		try:
-			http = httplib.HTTPConnection(self._host, self._port, self._port)
-			http.request("GET", url)
-			response = http.getresponse()
+    def _do_http_request(self, url):
+        http, content, msg, error, status = None, None, None, None, None
+        try:
+            http = httplib.HTTPConnection(self._host, self._port, self._port)
+            http.request("GET", url)
+            response = http.getresponse()
 
-			status = response.status
-			if status / 100 == 2:
-				content = self._decode_msg(response.read())
-			else:
-				msg = response.reason
-				error = self._decode_msg(response.read())
-		except (httplib.HTTPException, socket.error, socket.timeout) as e:
-			raise ZhiHuClientException(str(e))
-		except Exception as e:
-			raise ZhiHuClientException(str(e))
-		finally:
-			if http:
-				http.close()
-		if msg:
-			raise ZhiHuServiceException(status, msg, error)
+            status = response.status
+            if status / 100 == 2:
+                content = self._decode_msg(response.read())
+            else:
+                msg = response.reason
+                error = self._decode_msg(response.read())
+        except (httplib.HTTPException, socket.error, socket.timeout) as e:
+            raise ZhiHuClientException(str(e))
+        except Exception as e:
+            raise ZhiHuClientException(str(e))
+        finally:
+            if http:
+                http.close()
+        if msg:
+            raise ZhiHuServiceException(status, msg, error)
 
-		return content
+        return content
 
 if __name__ == "__main__":
-	zh = ZhiHu()
-	print zh.get_latest_news()
+    zh = ZhiHu()
+    print zh.get_latest_news()
--- handler.py
+++ handler.py
@@ -14,115 +14,115 @@ import search
 
 
 class BaseHandler(tornado.web.RequestHandler):
-	"""定义公共函数
-	"""
+    """定义公共函数
+    """
 
-	def get_error_html(self, status_code, **kwargs):
-		reason = "Server Error" if not hasattr(self, "_reason") else self._reason
-		exception = kwargs.get('exception', "")
+    def get_error_html(self, status_code, **kwargs):
+        reason = "Server Error" if not hasattr(self, "_reason") else self._reason
+        exception = kwargs.get('exception', "")
 
-		return self.render_string("error.html", code=str(status_code),
-		                          reason=str(reason), exception=str(exception))
+        return self.render_string("error.html", code=str(status_code),
+                                  reason=str(reason), exception=str(exception))
 
 
 class OperationHandler(BaseHandler):
-	"""clean data
-	"""
+    """clean data
+    """
 
-	def get(self, *args, **kwargs):
-		secret = self.get_argument("secret", "")
-		m = hashlib.md5()
-		m.update(secret)
-		if m.hexdigest() != config.secret:
-			self.set_status(403)
-			self.write('{"code": 403, "msg": "secret wrong"}')
-		else:
-			path = self.request.path
-			if path not in operation.operation_route.get_operation_routes():
-				self.set_status(404)
-				self.write('{"code": 404, "msg": "no operation"}')
-			else:
-				method = operation.operation_route.get_operation_routes()[path]
-				try:
-					method(self.request.arguments)
-				except Exception as e:
-					self.set_status(500)
-					self.write('{"code": 500, "msg": "%s"}' % str(e))
-				else:
-					self.set_status(200)
-					self.write('{"code": 200, "msg": "success"}')
+    def get(self, *args, **kwargs):
+        secret = self.get_argument("secret", "")
+        m = hashlib.md5()
+        m.update(secret)
+        if m.hexdigest() != config.secret:
+            self.set_status(403)
+            self.write('{"code": 403, "msg": "secret wrong"}')
+        else:
+            path = self.request.path
+            if path not in operation.operation_route.get_operation_routes():
+                self.set_status(404)
+                self.write('{"code": 404, "msg": "no operation"}')
+            else:
+                method = operation.operation_route.get_operation_routes()[path]
+                try:
+                    method(self.request.arguments)
+                except Exception as e:
+                    self.set_status(500)
+                    self.write('{"code": 500, "msg": "%s"}' % str(e))
+                else:
+                    self.set_status(200)
+                    self.write('{"code": 200, "msg": "success"}')
 
 
 class DayHandler(BaseHandler):
-	"""用于获取某一天的新闻
-	"""
-	def __init__(self, application, request, **kwargs):
-		super(DayHandler, self).__init__(application, request, **kwargs)
-		self._db = database.Dao()
+    """用于获取某一天的新闻
+    """
+    def __init__(self, application, request, **kwargs):
+        super(DayHandler, self).__init__(application, request, **kwargs)
+        self._db = database.Dao()
 
-	def get(self, *args, **kwargs):
-		default_date_str = datetime.datetime.now().strftime("%Y%m%d")
-		date_str = self.get_argument("date", default_date_str)
-		news_list = self._db.select_news_list(date_str)
+    def get(self, *args, **kwargs):
+        default_date_str = datetime.datetime.now().strftime("%Y%m%d")
+        date_str = self.get_argument("date", default_date_str)
+        news_list = self._db.select_news_list(date_str)
 
-		after_date = None if date_str == default_date_str \
-			else after_date_str(date_str)
+        after_date = None if date_str == default_date_str \
+            else after_date_str(date_str)
 
-		# empty
-		if len(news_list) == 0 and date_str == default_date_str:
-			date_str = before_date_str(default_date_str)
-			news_list = self._db.select_news_list(date_str)
-			after_date = None
+        # empty
+        if len(news_list) == 0 and date_str == default_date_str:
+            date_str = before_date_str(default_date_str)
+            news_list = self._db.select_news_list(date_str)
+            after_date = None
 
-		self.render("day.html", now_date=now_date_str(date_str),
-		            before_date=before_date_str(date_str),
-		            after_date=after_date,
-		            news_list=news_list)
+        self.render("day.html", now_date=now_date_str(date_str),
+                    before_date=before_date_str(date_str),
+                    after_date=after_date,
+                    news_list=news_list)
 
 
 class SearchHandler(BaseHandler):
-	"""处理搜索
-	"""
-	def __init__(self, application, request, **kwargs):
-		super(SearchHandler, self).__init__(application, request, **kwargs)
-		self._fts = search.FTS()
+    """处理搜索
+    """
+    def __init__(self, application, request, **kwargs):
+        super(SearchHandler, self).__init__(application, request, **kwargs)
+        self._fts = search.FTS()
 
-	def get(self, *args, **kwargs):
-		keywords = self.get_argument("keywords")
-		results = self._fts.search(keywords, limit=10)
-		self.render("search.html", results=results)
+    def get(self, *args, **kwargs):
+        keywords = self.get_argument("keywords")
+        results = self._fts.search(keywords, limit=10)
+        self.render("search.html", results=results)
 
 
 class ErrorHandler(BaseHandler):
-	"""处理错误信息
-	"""
+    """处理错误信息
+    """
 
-	def prepare(self):
-		self.render("404.html")
+    def prepare(self):
+        self.render("404.html")
 
 
 def before_date_str(date_str):
-	"""计算前一天的date_str
+    """计算前一天的date_str
 
-	:param date_str:
-	:return:
-	"""
-	now_date = datetime.datetime.strptime(date_str, "%Y%m%d")
-	before_date = now_date - datetime.timedelta(days=1)
-	return before_date.strftime("%Y%m%d")
+    :param date_str:
+    :return:
+    """
+    now_date = datetime.datetime.strptime(date_str, "%Y%m%d")
+    before_date = now_date - datetime.timedelta(days=1)
+    return before_date.strftime("%Y%m%d")
 
 def after_date_str(date_str):
-	"""计算后一天的date_str
+    """计算后一天的date_str
 
-	:param date_str:
-	:return:
-	"""
-	now_date = datetime.datetime.strptime(date_str, "%Y%m%d")
-	before_date = now_date + datetime.timedelta(days=1)
-	return before_date.strftime("%Y%m%d")
+    :param date_str:
+    :return:
+    """
+    now_date = datetime.datetime.strptime(date_str, "%Y%m%d")
+    before_date = now_date + datetime.timedelta(days=1)
+    return before_date.strftime("%Y%m%d")
 
 def now_date_str(date_str):
-	if datetime.datetime.now().strftime("%Y%m%d") == date_str:
-		return "今日热闻"
-	else:
-		return date_str
+    if datetime.datetime.now().strftime("%Y%m%d") == date_str:
+        return "今日热闻"
+    else:
+        return date_str
