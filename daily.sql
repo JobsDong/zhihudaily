9b76d056e1f77efdda245667286f453897419161
wuyadong <wuyadong311521@gmail.com>
2014-12-26 01:35:03 +0800

    add index for news table

--- daily.sql
+++ daily.sql
@@ -4,8 +4,12 @@ CREATE TABLE IF NOT EXISTS `news` (
 `title` varchar(100) NOT NULL,
 `share_url` varchar(100) NOT NULL,
 `date` varchar(50) NOT NULL,
-`body` text NOT NULL,
+`body` longtext NOT NULL,
 `image` varchar(100) NOT NULL,
 `image_source` varchar(100) NOT NULL,
 `image_public_url` varchar(100) NOT NULL
 ) DEFAULT CHARSET=utf8;
+
+CREATE INDEX date_index USING BTREE ON `news`(`date`);
+
+CREATE INDEX news_id_index USING BTREE ON `news`(`news_id`);
